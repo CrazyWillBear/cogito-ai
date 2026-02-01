c16fe47c6c51347837850667ce152f5cca146741
William Chastain <williamchastain2005@gmail.com>
2026-02-01 14:52:16 -0500

    Remove unncessary directories and files

--- cli/conversations.py
+++ cli/conversations.py
@@ -6,6 +6,7 @@ def write_logs(logs: str):
     """Write conversation logs to disk."""
 
     timestamp = time.strftime("%Y%m%d-%H%M%S")
-    p = Path(f"~/.cogito/conversations/conversation-{timestamp}.json")
+
+    p = Path.home() / Path(f".cogito/conversations/conversation-{timestamp}.json")
     p.parent.mkdir(parents=True, exist_ok=True)
     p.write_text(logs, encoding="utf-8")
